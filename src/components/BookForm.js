694dd398ae33827396e63d53d0b48b803adb9b1a
Isma <didouhismael@gmail.com>
2020-05-28 22:59:30 +0200

    Add book and reset input values

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,12 +2,19 @@ import React, { useContext, useState } from "react";
 import { BookContext } from "../contexts/BookContext";
 
 const BookForm = () => {
-  const { addBook } = useContext(BookContext);
+  const { books, addBook } = useContext(BookContext);
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    addBook(title, author);
+    resetInputs();
+  };
+
+  const resetInputs = () => {
+    setTitle("");
+    setAuthor("");
   };
 
   return (
