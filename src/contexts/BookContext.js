c74d3a687400a07a6e195da289ad9f2a88e5acc9
Isma <didouhismael@gmail.com>
2020-05-28 22:16:26 +0200

    book context

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,10 @@
+import React, { createContext, useState } from "react";
+
+export const BookContext = createContext();
+
+const BookContextProvider = (props) => {
+  const [books, setBooks] = useState([
+    { title: "Name of the wind", author: "Patrick Rothfuss", id: 1 },
+    { title: "The final empire", author: "Brandon Sanderson", id: 2 },
+  ]);
+};
